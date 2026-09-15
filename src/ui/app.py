e760160549b1471a0fb251295411c28378e5e6c0
Cinderyan <taiwanryan960@gmail.com>
2026-09-15 13:57:24 +0800

    minor tweak to sections

--- src/ui/app.py
+++ src/ui/app.py
@@ -20,19 +20,55 @@ def run_app(page: ft.Page):
         controls = [
             title,
             view_calendar_button    
-        ]
+        ],
+        alignment = ft.MainAxisAlignment.SPACE_BETWEEN
     )
     calendar_section = ft.Container(
-        content = ft.Text("今日代辦事項"),
-        padding = 20
+        content = ft.Column(
+            controls = [
+                ft.Text(
+                    "今日代辦事項",
+                    size = 20
+                ),
+                ft.Text("目前沒有代辦事項")
+            ]
+        ),
+        padding = 20,
+        border_radius = 12,
+        bgcolor = ft.Colors.SURFACE_CONTAINER
     )
     deadline_section = ft.Container(
-        content = ft.Text("將於今日截止的事件"),
-        padding = 20
+        content = ft.Column(
+            controls = [
+                ft.Text(
+                    "即將到期事件",
+                    size = 20
+                ),
+                ft.Text("目前沒有即將到期事件")
+            ]
+        ),
+        padding = 20,
+        border_radius = 12,
+        bgcolor = ft.Colors.SURFACE_CONTAINER
     )
     time_tracker_section = ft.Container(
-        content = ft.Text("今日時間紀錄"),
-        padding = 20
+        content = ft.Column(
+            controls = [
+                ft.Text(
+                    "時間紀錄",
+                    size = 20
+                ),
+                ft.Text(
+                    "Study: 0 hr"
+                ),
+                ft.Text(
+                    "Work: 0 hr"
+                )
+            ]
+        ),
+        padding = 20,
+        border_radius = 12,
+        bgcolor = ft.Colors.SURFACE_CONTAINER
     )
     home = ft.Column(
         controls = [
